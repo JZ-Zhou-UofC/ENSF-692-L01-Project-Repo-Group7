e2fef9b17e0308595e062f5021a9f6aa13d471ca
John Zhou <john.zhou1@ucalgary.ca>
2025-06-10 23:41:30 -0600

    naming convention fix

--- Data_Handling.py
+++ Data_Handling.py
@@ -2,7 +2,6 @@
 # John Zhou & Jack Shenfield
 
 # imported libraries
-import re
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,7 +12,7 @@ import os
 # Takes values like 42k and converts to 42,000
 # Works for thousands (k), millions (m), billions(b)
 # some logic inspired by chatgpt here.
-def convert_to_no(val):
+def convert_to_number(val):
     try:
         if isinstance(val, str):
             val = val.strip().lower() # removes whitespace, makes any letters lowercase
@@ -37,16 +36,15 @@ def convert_to_no(val):
 
 # slice data into only the selected countries
 # Not meant to be called directly, but just used in datacombiner()
-def data_processing(excelfile, selected_countries):
+def data_processing(excel_file, selected_countries):
 
     # Read data and make dataframe
-    raw_data = pd.read_excel(excelfile)
+    raw_data = pd.read_excel(excel_file)
     data = pd.DataFrame(raw_data)
 
     # Extract filename to be added to data eventually
-    filename = os.path.basename(excelfile)
+    filename = os.path.basename(excel_file)
     data['type'] = filename
-
 
     # list comprehension for normalizing country names to lowercase
     selected_countries = [country.lower() for country in selected_countries]
@@ -56,30 +54,30 @@ def data_processing(excelfile, selected_countries):
     # use convert to number function on numerical columns
     for col in data.columns:
         if col not in ['country', 'type']:
-            data[col] = data[col].apply(convert_to_no)
+            data[col] = data[col].apply(convert_to_number)
 
     # null values check in dataframe. If any null values exist, return True
-    nullvalues = False
-    nullvalues = bool(data.isnull().values.any()) # return python boolean, not np.boolean
+    null_values = False
+    null_values = bool(data.isnull().values.any()) # return python boolean, not np.boolean
 
     # numberical values check in dataframe. If any non-numericals exsit, return True
-    numericalvalues = False
+    numerical_values = False
     for col in data.columns: # check for non-numeric values in the the columns that should be numeric
         if col not in ['country', 'type']:  # skip non-numeric columns
             converted = pd.to_numeric(data[col], errors='coerce') # Force conversion to a numeric value, coercing errors to NaN
             if converted.notnull().sum() != data[col].notnull().sum(): # Compare initial of non-nulls to converted column
-                numericalvalues = True
+                numerical_values = True
                 break
 
     # Check if null values or numerical values were found
-    if (nullvalues or numericalvalues):
+    if (null_values or numerical_values):
         print("There are either null values or non-numerical values in the data.")
         return data # return the data either way. It can still be used even if it is missing some values.
     else:
         return data
     
 # combine the three dataframes into one larger dataframe
-def datacombiner(data1, data2, data3, country_list):
+def data_combiner(data1, data2, data3, country_list):
     try:
         data1 = data_processing(data1, country_list)
         data2 = data_processing(data2, country_list)
@@ -94,22 +92,22 @@ def datacombiner(data1, data2, data3, country_list):
     
 
 # prompts for user input and extracts the data for the country.
-def extractinitialcountry(dataframe):
+def extract_initial_country(dataframe):
     while True: # loops until valid input is received
-        userinput = input("Please enter a country in the G20: ") # ask for input
+        user_input = input("Please enter a country in the G20: ") # ask for input
 
-        userinput = userinput.lower()# normalizes the input string
-        datalower = dataframe['country'].str.lower() # lowercase the breeds for comparison
+        user_input = user_input.lower()# normalizes the input string
+        data_lower = dataframe['country'].str.lower() # lowercase the breeds for comparison
 
-        if userinput in datalower.values:
-            return(dataframe[datalower == userinput]) # return data in dataframe where datalower is equal to userinput
+        if user_input in data_lower.values:
+            return(dataframe[data_lower == user_input]) # return data in dataframe where datalower is equal to userinput
         else:
             print("Country not found in G20 data. Please try again.")
             
 
 # List of G20 countries
 # Not including European Union, as it is not in the data
-G20_list = ["Argentina", "Australia", "Brazil", "Canada", "China", "France",
+G20_LIST = ["Argentina", "Australia", "Brazil", "Canada", "China", "France",
             "Germany", "India", "Indonesia", "Italy", "Japan", "Mexico", "Russia",
             "Saudi Arabia", "South Africa", "South Korea", "Turkey", "United Kingdom",
             "United States"]
@@ -117,9 +115,10 @@ G20_list = ["Argentina", "Australia", "Brazil", "Canada", "China", "France",
 
 def main():
 
-    dataframe = datacombiner("lex.xlsx", "gdp_pcap.xlsx", "pop.xlsx", G20_list)
+    dataframe = data_combiner("lex.xlsx", "gdp_pcap.xlsx", "pop.xlsx", G20_LIST)
 
     print(dataframe)
+    print(extract_initial_country(dataframe))
 
 
 if __name__ == '__main__':
