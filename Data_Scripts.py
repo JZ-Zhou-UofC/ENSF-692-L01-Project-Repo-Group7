757a8b392095192505269af4f3fe2221d21e7d21
jshenfield <jbshenfield@gmail.com>
2025-06-13 15:06:48 -0600

    Added a couple more files

--- Data_Scripts.py
+++ Data_Scripts.py
@@ -31,3 +31,37 @@ filtered = filtered[(filtered["REF_DATE"] >= "2005-01")]
 filtered = filtered[["REF_DATE", "GEO", "VALUE"]]
 
 print(filtered)
+
+#print(filtered)
+
+# Consumer Price Index data processing
+
+raw_data = pd.read_csv("ConsumerPriceIndexData.csv")
+df = pd.DataFrame(raw_data)
+
+filtered = df[df["GEO"].isin(Provinces)]
+filtered = filtered[(filtered["REF_DATE"] <= "2025-01")]
+filtered = filtered[(filtered["REF_DATE"] >= "2005-01")]
+filtered = filtered[(filtered["UOM"] == "2002=100")]
+filtered = filtered[(filtered["Products and product groups"] == "All-items")]
+filtered = filtered[["REF_DATE", "GEO", "VALUE"]]
+
+print(filtered)
+
+
+# Wage & Salary data processing
+
+raw_data = pd.read_csv("WagesData.csv")
+df = pd.DataFrame(raw_data)
+
+filtered = df[df["GEO"].isin(Provinces)]
+filtered = filtered[(filtered["REF_DATE"] <= "2025-01")]
+filtered = filtered[(filtered["REF_DATE"] >= "2005-01")]
+filtered = filtered[(filtered["Seasonal adjustment"] == "Unadjusted")]
+filtered = filtered[(filtered["Sector"] == "Compensation of employees")]
+filtered["VALUE"] = filtered["VALUE"] * 1000 # Values are in units of thousands of dollars
+filtered = filtered[["REF_DATE", "GEO", "VALUE"]]
+
+print(filtered)
+
+
