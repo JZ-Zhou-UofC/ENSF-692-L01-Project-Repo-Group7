e5178b869dba7520bb3f1dbb920b76ec4b0ebe70
John Zhou <john.zhou1@ucalgary.ca>
2025-06-14 19:40:29 -0600

    re organzied the function and added some comments

--- cli.py
+++ cli.py
@@ -1,18 +1,12 @@
 from data_processing import *
 
-def run_cli():
+def run_cli(df):
     print("Welcome to the Data Analysis Project")
 
-
-
-    df=create_dataframe()
-    print(df.head())
-    df=create_multi_indexing(df)
-   
-    df=adding_average_monthly_wage_column(df)
     print("_____________________________add column____________")
     print(df)
-    
+    # example on how to generate a graph
+    # these 2 things will be acquired from the user input. this gives the user option to show different graphs
     input_province_array=["Alberta","British Columbia"]
     covid_period=True
     create_graph_to_compare_migration_trends_for_two_province(input_province_array,df,covid_period)
